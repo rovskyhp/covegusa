5e155f7dfbd696dcdc1dec7adf4bf5e8c5a68e71
Adrian Herrera <rovskyhp@gmail.com>
2014-06-22 00:18:36 -0500

    Added API for usuarios table

--- api/index.php
+++ api/index.php
@@ -1,52 +1,31 @@
 <?php
-/**
- * Step 1: Require the Slim Framework
- *
- * If you are not using Composer, you need to require the
- * Slim Framework and register its PSR-0 autoloader.
- *
- * If you are using Composer, you can skip this step.
- */
+
 require 'Slim/Slim.php';
 
-\Slim\Slim::registerAutoloader();
+use Slim\Slim;
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
-$app = new \Slim\Slim();
+Slim::registerAutoloader();
 
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
+$app = new Slim();
 
 $app->get('/facturas', 'getFacturas');
 $app->get('/facturas/:id', 'getFactura');
-$app->get('/facturas/search/:query', 'findByRFC');
+$app->get('/facturas/search/:query', 'findFacturaByRFC');
 $app->post('/facturas', 'addFactura');
 $app->put('/facturas/:id', 'updateFactura');
 $app->delete('/facturas/:id',  'deleteFactura');
 
-/**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This executes the Slim application
- * and returns the HTTP response to the HTTP client.
- */
+$app->get('/usuarios', 'getUsuarios');
+$app->get('/usuarios/:id', 'getUsuario');
+$app->get('/usuarios/search/:query', 'findUsuarioByRFC');
+$app->post('/usuarios', 'addUsuario');
+$app->put('/usuarios/:id', 'updateUsuario');
+$app->delete('/usuarios/:id', 'deleteUsuario');
+
 $app->run();
 
 function getFacturas(){
-    $sql = "select * FROM facturas ORDER BY fecha_correspondiente";
+    $sql = "SELECT * FROM facturas ORDER BY fecha_correspondiente";
         try {
             $db = getConnection();
             $stmt = $db->query($sql);
@@ -74,10 +53,8 @@ function getFactura($id) {
 }
 
 function addFactura() {
-    $request = \Slim\Slim::getInstance()->request();
-    $factura = json_decode($request->getBody());
-    var_dump($factura);
-    exit;
+    $request = Slim::getInstance()->request();
+    $factura = json_decode( $request->getBody());
     $sql = "INSERT INTO facturas (id_usuario, nombre_nota, nombre_pdf, nombre_xml, rfc_cliente, fecha_correspondiente) VALUES (:id_usuario, :nombre_nota, :nombre_pdf, :nombre_xml, :rfc_cliente, :fecha_correspondiente)";
     try {
         $db = getConnection();
@@ -98,7 +75,7 @@ function addFactura() {
 }
 
 function updateFactura($id) {
-    $request = \Slim\Slim::getInstance()->request();
+    $request = Slim::getInstance()->request();
     $body = $request->getBody();
     $factura = json_decode($body);
     $sql = "UPDATE facturas SET id_usuario=:id_usuario, nombre_nota=:nombre_nota, nombre_pdf=:nombre_pdf, nombre_xml=:nombre_xml, rfc_cliente=:rfc_cliente, fecha_correspondiente=:fecha_correspondiente WHERE id_factura=:id";
@@ -134,7 +111,7 @@ function deleteFactura($id) {
     }
 }
 
-function findByRFC($query) {
+function findFacturaByRFC($query) {
     $sql = "SELECT * FROM facturas WHERE UPPER(rfc_cliente) LIKE :query ORDER BY rfc_cliente";
     try {
         $db = getConnection();
@@ -150,12 +127,112 @@ function findByRFC($query) {
     }
 }
 
+function getUsuarios(){
+    $sql = "SELECT * FROM usuarios ORDER BY nombre";
+        try {
+            $db = getConnection();
+            $stmt = $db->query($sql);
+            $usuarios = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+            echo '{"usuarios": ' . json_encode($usuarios) . '}';
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+}
+
+function getUsuario($id){
+    $sql = "SELECT * FROM usuarios WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $usuario = $stmt->fetchObject();
+        $db = null;
+        echo json_encode($usuario);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+
+function addUsuario(){
+    $request = Slim::getInstance()->request();
+    $usuario = json_decode( $request->getBody());
+    $sql = "INSERT INTO usuarios (nombre, pass, rfc, user) VALUES (:nombre, :pass, :rfc, :user)";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("nombre", $usuario->nombre);
+        $stmt->bindParam("pass", $usuario->pass);
+        $stmt->bindParam("rfc", $usuario->rfc);
+        $stmt->bindParam("user", $usuario->user);
+        $stmt->execute();
+        $usuario->id = $db->lastInsertId();
+        $db = null;
+        echo json_encode($usuario);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function updateUsuario($id){
+    $request = Slim::getInstance()->request();
+    $body = $request->getBody();
+    $usuario = json_decode($body);
+    $sql = "UPDATE usuarios SET nombre=:nombre, pass=:pass, rfc=:rfc, user=:user WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("nombre", $usuario->nombre);
+        $stmt->bindParam("pass", $usuario->pass);
+        $stmt->bindParam("rfc", $usuario->rfc);
+        $stmt->bindParam("user", $usuario->user);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $db = null;
+        echo json_encode($usuario);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function deleteUsuario($id){
+    $sql = "DELETE FROM usuarios WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $db = null;
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function findUsuarioByRFC($query){
+    $sql = "SELECT * FROM usuarios WHERE UPPER(rfc)=UPPER(:query) ORDER BY rfc";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $query = $query;
+        $stmt->bindParam("query", $query);
+        $stmt->execute();
+        $usuario = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"usuario": ' . json_encode($usuario) . '}';
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
 function getConnection() {
     $dbhost="127.0.0.1";
     $dbuser="covegusa_user";
     $dbpass="c0v3gusQ";
     $dbname="covegusa_db";
-    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 }
