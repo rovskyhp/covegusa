1f024fd06da4d5fd5e23bd449bdd6e2e6a959b04
Adrian Herrera <rovskyhp@gmail.com>
2014-07-29 20:19:33 -0500

    Added welcome file with styles

--- welcome.php
+++ welcome.php
@@ -1,16 +1,19 @@
+<?php
+  include('lock.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head> 
+  <head>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
+  <script src="js/data_table.js"></script>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/data_table.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico">
   </head>
-  <?php
-  include('lock.php');
-  ?>
 <style>
 body {
   padding-top: 50px;
@@ -21,7 +24,6 @@ body {
 }
 </style>
 <body>
-
   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
 
@@ -29,22 +31,58 @@ body {
           <span class="navbar-brand">Bienvenido: <?php echo $login_session; ?></span>
         </div>
         <div class="collapse navbar-collapse">
-          
           <ul class="nav navbar-nav" style="float:right">
-            <li class="active"><a href="#">Home</a></li>
             <li><a href="logout.php">Salir</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
   </div>
- 
+
     <div class="container">
 
       <div class="starter-template">
-        <h1>Informaci&oacute;n del Usuario</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
+        <h3>RFC: <?echo $rfc_session ?></h3>
+
+
+<table id="example" class="display" cellspacing="0" width="100%">
+    <thead>
+        <tr>
+            <th>ID factura</th>
+            <th>ID Usuario</th>
+            <th>Fecha</th>
+            <th>Fecha de creaci&oacute;n</th>
+            <th>RFC</th>
+            <th>PDF</th>
+            <th>XML</th>
+            <th>Notas</th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <th>ID factura</th>
+            <th>ID Usuario</th>
+            <th>Fecha</th>
+            <th>Fecha de creaci&oacute;n</th>
+            <th>RFC</th>
+            <th>PDF</th>
+            <th>XML</th>
+            <th>Notas</th>
+        </tr>
+    </tfoot>
+
+</table>
+</div>
 
     </div><!-- /.container -->
 
+<script>
+
+$(document).ready(function() {
+    var url= "/api/facturas/search/<?php echo $rfc_session?>"
+    $('#example').dataTable( {
+         "sAjaxSource": url
+    } );
+} );
+
+</script>
 </body>
